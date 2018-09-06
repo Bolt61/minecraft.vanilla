f3d59d10e114fd9cfc845e5f404d627b81a1b447
Nartr <bolt61.minecraft@gmail.com>
2018-09-06 21:27:25 +0200

    Initial Commit

---
+++ src/main/java/ch/bolt61/vanillaserver/Main.java
@@ -0,0 +1,12 @@
+package ch.bolt61.vanillaserver;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		super.onEnable();
+		System.out.println("Plugin enabled");
+	}
+}
